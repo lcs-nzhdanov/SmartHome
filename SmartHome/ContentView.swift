18500c33e6b0a671612a920f4c25b678de5eedfd
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-01-09 20:53:25 -0500

    Initial Commit

---
+++ SmartHome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartHome
+//
+//  Created by Nikita Zhdanov on 2024-01-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartHome/SmartHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeApp.swift
+//  SmartHome
+//
+//  Created by Nikita Zhdanov on 2024-01-09.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
